af286cf8a96753c19eab8a171fe27b71a6f3166b
Vieux Maiga <mvieux016@gmail.com>
2025-03-17 09:39:48 +0000

    debuter

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,39 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-void main() {
-  runApp(AdminLogin());
+void main(){
+  return runApp(PageAcceuil());
 }
 
-class AdminLogin extends StatelessWidget {
-  const AdminLogin({super.key});
+class PageAcceuil extends StatelessWidget {
+  const PageAcceuil({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+
+    return MaterialApp(
+      home:Scaffold (
+      appBar: AppBar(
+        title: Text("Nom  Prénom"),    
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+
+            ElevatedButton(onPressed: (){}, child: Text("Gestion des candidats")),
+            ElevatedButton(onPressed: (){}, child: Text("Gestion des resultats")),
+            ElevatedButton(onPressed: (){}, child: Text("Gestion des listes")),
+            ElevatedButton(onPressed: (){}, child: Text("Infos concours")),
+
+          ],
+        ),
+      ),
+      body:// ListView.builder(itemBuilder: itemBuilder),
+      ListView(
+       
+      )
+    )
+    );
   }
 }
