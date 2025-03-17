6f3177448e7f0f28e4ae483816b566d012a311b4
Vieux Maiga <mvieux016@gmail.com>
2025-03-17 08:53:53 +0000

    Envoyer

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(AdminLogin());
+}
+
+class AdminLogin extends StatelessWidget {
+  const AdminLogin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
